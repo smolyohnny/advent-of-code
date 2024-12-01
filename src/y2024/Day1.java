c9bb47ead8803096e4cf3e3dc2dde19693862735
yohnny <114059682+smolyohnny@users.noreply.github.com>
2024-12-01 06:34:01 +0100

    y2024 Day1

--- src/y2024/Day1.java
+++ src/y2024/Day1.java
@@ -1,5 +1,55 @@
 package y2024;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Day1 {
-    //1.12.2024 6:00
+
+    private static final Path path = Path.of("src\\y2024\\inputs\\day1.txt");
+    private static List<String> input = new ArrayList<>();
+    private static final List<Integer> col1 = new ArrayList<>();
+    private static final List<Integer> col2 = new ArrayList<>();
+    private static int sum1 = 0;
+    private static int sum2 = 0;
+
+    public static void main(String[] args) throws IOException {
+        input = Files.readAllLines(path);
+
+        fillLists();
+        solvePart1();
+        solvePart2();
+    }
+
+    private static void solvePart1() {
+        for (int i = 0; i < col1.size(); i++) {
+            sum1 += Math.abs(col1.get(i) - col2.get(i));
+        }
+        System.out.println(sum1);
+    }
+
+    private static void solvePart2() {
+        for (int i : col1) {
+            int appearances = 0;
+            for (int j : col2) {
+                if (i == j) appearances++;
+            }
+            sum2 += (i * appearances);
+        }
+        System.out.println(sum2);
+    }
+
+    private static void fillLists() {
+        for (String line : input) {
+            String[] arr = line.split(" {3}", 0);
+            col1.add(Integer.parseInt(arr[0]));
+            col2.add(Integer.parseInt(arr[1]));
+        }
+        Collections.sort(col1);
+        Collections.sort(col2);
+    }
+
 }
